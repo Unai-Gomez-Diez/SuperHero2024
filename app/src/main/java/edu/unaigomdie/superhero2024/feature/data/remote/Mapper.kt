654127e4d6731d80ea79e541a580c83fd224171a
Unai-Gomez-Diez <unaigomez0807@gmail.com>
2024-09-24 12:12:33 +0200

    GetById con retrofit

--- app/src/main/java/edu/unaigomdie/superhero2024/feature/data/remote/Mapper.kt
+++
@@ -1,6 +0,0 @@
-package edu.unaigomdie.superhero2024.feature.data.remote
-
-import edu.unaigomdie.superhero2024.feature.domain.SuperHero
-
-fun SuperHero.toModel(): SuperHero=
-    SuperHero(this.id, this.name, this.slug, this.powerStats, this.appearance, this.biography, this.work, this.images)
