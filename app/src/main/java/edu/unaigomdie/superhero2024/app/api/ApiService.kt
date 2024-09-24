0f7c8d4fb1c897c2f7ddafc770e68d7b8174c5d1
Unai-Gomez-Diez <unaigomez0807@gmail.com>
2024-09-24 15:21:41 +0200

    superhero avance

--- app/src/main/java/edu/unaigomdie/superhero2024/app/api/ApiService.kt
+++ app/src/main/java/edu/unaigomdie/superhero2024/app/api/ApiService.kt
@@ -1,11 +1,9 @@
 package edu.unaigomdie.superhero2024.app.api
 
-import edu.unaigomdie.superhero2024.feature.domain.Biography
 import edu.unaigomdie.superhero2024.feature.domain.SuperHero
-import edu.unaigomdie.superhero2024.feature.domain.Work
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
-import retrofit2.Response
 
 interface ApiService {
     @GET("all.json")
