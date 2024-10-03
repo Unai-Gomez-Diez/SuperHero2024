500aa36449357eb141648fb904f81a339510da4d
Unai-Gomez-Diez <unaigomez0807@gmail.com>
2024-10-03 16:41:16 +0200

    uistate

--- app/src/main/java/edu/unaigomdie/superhero2024/feature/presentation/SuperHeroesViewModel.kt
+++ app/src/main/java/edu/unaigomdie/superhero2024/feature/presentation/SuperHeroesViewModel.kt
@@ -1,16 +1,34 @@
 package edu.unaigomdie.superhero2024.feature.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import edu.unaigomdie.superhero2024.feature.domain.GetSuperHeroesUseCase
 import edu.unaigomdie.superhero2024.feature.domain.SuperHero
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class SuperHeroesViewModel(
     private val getSuperHeroesUseCase: GetSuperHeroesUseCase
 ) :
     ViewModel() {
-    suspend fun getSuperHeroes(): List<SuperHero> {
-        return getSuperHeroesUseCase.invoke()
+
+    private val _uistate = MutableLiveData<UiState>()
+    val uiState: LiveData<UiState> = _uistate
+
+    suspend fun getSuperHeroes() {
+        GlobalScope.launch(Dispatchers.IO){
+            UiState(superHeroes = getSuperHeroesUseCase.invoke())
+        }
+
+
+
     }
+
+    data class UiState(
+        val superHeroes: List<SuperHero> = emptyList()
+    )
 
 
 
