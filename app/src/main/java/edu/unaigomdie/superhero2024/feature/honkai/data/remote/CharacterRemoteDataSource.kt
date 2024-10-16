0e404c06acf01c3135c974633673ecd7f476e5f9
Unai-Gomez-Diez <unaigomez0807@gmail.com>
2024-10-16 16:59:28 +0200

    no funciona el svg

--- app/src/main/java/edu/unaigomdie/superhero2024/feature/honkai/data/remote/CharacterRemoteDataSource.kt
+++ app/src/main/java/edu/unaigomdie/superhero2024/feature/honkai/data/remote/CharacterRemoteDataSource.kt
@@ -8,31 +8,49 @@ import io.ktor.client.call.body
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import io.ktor.client.statement.content
+import io.ktor.client.statement.readRawBytes
+import io.ktor.client.statement.request
 import io.ktor.http.ContentType.Application.Json
 import io.ktor.serialization.kotlinx.json.json
+import io.ktor.utils.io.InternalAPI
+import io.ktor.utils.io.readUTF8Line
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromStream
 
 class CharacterRemoteDataSource {
     val baseUrl = "https://hsr-api.vercel.app/api/v1/"
     val apiClient = ApiClient(baseUrl)
 
+    @OptIn(InternalAPI::class)
     suspend fun getCharacters(): List<Character> {
         apiClient.retrofit
         val response = apiClient.apiService.getCharacters()
         //return response.body() ?: emptyList()
-        val client = HttpClient(CIO){
+        val client = HttpClient(){
             install(ContentNegotiation) {
                 json(Json {
                     ignoreUnknownKeys = true
                 })
             }
         }
-        val response2 = client.get("http://10.0.2.2:8080/characters")
-        Log.d("@dev",response2.body<List<Character>>().toString())
+
+
+        val json = Json { ignoreUnknownKeys = true }
+
+        val response2 = client.get("http://10.0.2.2:8080/characters").readRawBytes()
+        val jsonString = String(response2, Charsets.UTF_8)
+        val characters: List<Character> = json.decodeFromString(jsonString)
+        Log.d("@dev", characters.toString())
 
 
 
-        return response.body() ?: emptyList()
+        return characters ?: emptyList()
+
+
+
+
 
 
 
